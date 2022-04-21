5a7c723cfd1c57440782e129fedea517d07f9be3
RikkaW <rikka@shizuku.moe>
2022-04-21 20:05:44 +0800

    Bump version to 1.3.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ task("clean", type = Delete::class) {
 
 subprojects {
     group = "dev.rikka.tools.materialthemebuilder"
-    version = "1.3.1"
+    version = "1.3.2"
 
     plugins.withId("java") {
         println("- Configuring `java`")
